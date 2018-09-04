66fc919cb80f8065e4c1195d435f45305a2ebd6f
Maike <maikebr@gmail.com>
2018-09-04 12:59:40 -0300

    oi

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/main.cpp 
+
+OBJS += \
+./src/main.o 
+
+CPP_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	g++ -I"C:\Users\maike\Atollic\TrueSTUDIO\STM32_workspace_9.0\pcvision\include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+
